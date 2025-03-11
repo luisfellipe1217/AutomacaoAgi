2f9b21b86d440038f3e640f509dddff1aabb0562
Luis <luis.fellipe1217@gmail.com>
2025-03-11 12:19:14 -0300

    Configure ChromeDriver

--- src/test/java/support/WebDriverManager.java
+++ src/test/java/support/WebDriverManager.java
@@ -1,17 +1,25 @@
 package support;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 
 public class WebDriverManager {
 
-    public WebDriver driver = new EdgeDriver();
+    ChromeOptions options = new ChromeOptions();
+    public WebDriver driver = new ChromeDriver();
 
     public WebDriverManager(){
     }
 
     public void webDriverInit(){
-        System.setProperty("webdriver.edge.driver", "src/test/resources/webdriver/msedgedriver");
+
+        options.addArguments("--headless"); // Se estiver rodando sem interface gráfica
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--user-data-dir=" + System.getProperty("java.io.tmpdir") + "/chrome-user-data");
+        
         driver.manage().window().maximize();
         driver.manage().deleteAllCookies();
         driver.get("https://blogdoagi.com.br/");
