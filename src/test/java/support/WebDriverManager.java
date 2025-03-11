ab6cd1e0c014f803b31fbcf1c9bf36a84bf65521
Luis <luis.fellipe1217@gmail.com>
2025-03-11 16:12:26 -0300

    Rolling back to Chrome

--- src/test/java/support/WebDriverManager.java
+++ src/test/java/support/WebDriverManager.java
@@ -4,7 +4,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.edge.EdgeOptions;
 
 
 public class WebDriverManager {
@@ -12,8 +11,7 @@ public class WebDriverManager {
     private static WebDriver driver;
 
     public static WebDriver driverSetup(){
-       System.setProperty("webdriver.edge.driver", "src/test/resources/webdriver/msedgedriver");
-       return driver = new EdgeDriver();
+        return driver = new ChromeDriver();
     }
 
     public static void webDriverClose(){
